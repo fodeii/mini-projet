bd48fb96bf1b1de31e47fd15862d51350f866f24
fodeii <49885789+fodeii@users.noreply.github.com>
2021-04-24 16:23:55 +0000

    Update Accueil.java

--- Accueil.java
+++ Accueil.java
@@ -1,63 +1,97 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sn.supinfo.javapoo.miniprojet;
-import java.util.ArrayList;
+package sn.supinfo.javapoo.miniprojet.test;
+import java.util.List;
+import sn.supinfo.javapoo.miniprojet.classes.Groupe;
+import sn.supinfo.javapoo.miniprojet.classes.Etudiant;
+import sn.supinfo.javapoo.miniprojet.classes.GroupeManager;
+import sn.supinfo.javapoo.miniprojet.Controllers.EtudiantManagement;
+import sn.supinfo.javapoo.miniprojet.Menus;
+//import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  *
  * @author Muhammed Fode
- * Ceci est le fichier principale contenant le main.
  */
 public class Accueil {
     public static void main (String[] args)
     {
-     Scanner sc = new Scanner(System.in);
-     Menus.menuPrincipal();
-     int choix = sc.nextInt();
-     if (choix == 1)
+     while(true)
+     {
+        Menus.menuPrincipal();
+        Scanner sc = new Scanner(System.in);
+        int choix0 = sc.nextInt();
+        switch(choix0)
         {
-         premierePartie:
-         Menus.menuGroupe();
-         Scanner sc0 = new Scanner(System.in);
-         int choixG = sc0.nextInt();
-         switch (choixG){
-         case 1 :
-         {
-             int ID,effectif;
-             String nomGroupe;
-             Scanner sc1 = new Scanner(System.in);
-             System.out.println("Saisissez l'ID du nouveau groupe:");
-             ID = sc1.nextInt();
-             Scanner sc2 = new Scanner(System.in);
-             System.out.println("Saisissez 1e nom du nouveau groupe:");
-             nomGroupe = sc2.next();
-             Scanner sc3 = new Scanner(System.in);
-             System.out.println("Saisissez l'effectif");
-             effectif = sc3.nextInt();
-             Groupe.addGroup(ID,nomGroupe,effectif);
-             /*le break ci dessous ne fonctionne pas. Je voulais utiliser le concept d'etiquette
-             pour revenir a la ligne 24 mais j'ai toujours un erreur*/
-             break premierePartie;
-         }
-         
-         case 2 :
-         {
-             System.out.println("Entrez le nom du groupe a modifer: ");
-             Scanner sc4 = new Scanner(System.in);
-             String choix2 = sc4.next();
-         }
-         /*if (choix == 2)
-         {
-             System.out.println("Entrez le nom du groupe a modifer: ");
-             Scanner sc4 = new Scanner(System.in);
-             String choix2 = sc4.next();
-             
-         }*/
-        }
+            case 1:
+           {
+
+            Menus.menuGroupe();
+            //Scanner sc0 = new Scanner(System.in);
+            int choix1 = sc.nextInt();
+            switch (choix1)
+            {
+            case 1 :
+               {
+                System.out.println("Saisissez l'ID du nouveau groupe:");
+                int ID = sc.nextInt();
+                System.out.println("Saisissez 1e nom du nouveau groupe:");
+                String nomGroupe = sc.next();
+                System.out.println("Saisissez l'effectif");
+                int Effectif = sc.nextInt();
+                Groupe groupe = new Groupe(ID,nomGroupe,Effectif);
+                GroupeManager.addGroup(groupe);
+                break;
+               }
+            case 2 :
+                {
+                     GroupeManager.listGroupe();
+                }
+            }
+           }
+            case 2:
+            {
+                Menus.menuEtudiant();
+                EtudiantManagement EtudiantM = new EtudiantManagement();
+                   int choix1 = sc.nextInt();
+                   switch (choix1)
+                   {
+                   case 1 :
+                       {
+                          
+                          System.out.println("Saisissez l'ID de l'Etudiant:");
+                          int ID = sc.nextInt();
+                          System.out.println("Saisissez le prenom de l'Etudiant:");
+                          String prenom = sc.next();
+                          System.out.println("Saisissez le nom de l'Etudiant:");
+                          String nom = sc.next();
+                          System.out.println("Saisissez l'adresse de l'Etudiant:");
+                          String adresse = sc.next();
+                          System.out.println("Saisissez la date de naissance de l'Etudiant:");
+                          String date_de_naissance = sc.next();
+                          System.out.println("Saisissez le telephone de l'Etudiant:");
+                          String telephone = sc.next();
+                          System.out.println("Saisissez l'email l'Etudiant:");
+                          String email = sc.next();
+                          System.out.println("Saisissez la situation matrimoniale de l'Etudiant:");
+                          String situation_matrimoniale = sc.next();
+                          System.out.println("Saisissez la filiere de l'Etudiant:");
+                          String filiere = sc.next();
+                          Etudiant etudiant = new Etudiant(filiere,ID,prenom,nom,adresse,date_de_naissance,telephone,email,situation_matrimoniale);
+                          EtudiantM.addEtudiant(etudiant);
+                          break;
+                       }
+                   case 2 :
+                        {
+                           System.out.println("test");
+                           List<Etudiant>etudiants = EtudiantM.getAllEtudiant();
+                           //EtudiantM.listEtudiants();
+                        }
+
+                   }
+
+            }
+       }
+
+     }
     }
-  }
 }
